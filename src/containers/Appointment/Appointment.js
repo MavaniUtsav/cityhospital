5a82d8f7fe5035b9139475f860271c964d086eb9
srushti7787484 <srushtichangela267@gmail.com>
2023-09-29 17:51:45 +0530

    Done

--- src/containers/Appointment/Appointment.js
+++ src/containers/Appointment/Appointment.js
@@ -5,13 +5,34 @@ import { useFormik } from 'formik';
 
 function Appointment(props) {
 
+    const date = new Date()
+    date.setDate(date.getDate() - 1);
+
     const appointmentSchema = yup.object().shape({
-        name: yup.string().required('Please fill this field').matches(/^[a-zA-Z ]{3,15}$/, 'Please enter valid name'),
-        email: yup.string().email('Please enter valid email').required('Please fill this field'),
-        phone: yup.string().matches(/^[0-9]{10,10}$/, 'Phone number is not valid'),
-        date: yup.string().required('Please fill this field'),
+        name: yup.string().required('*Please fill this field').matches(/^[a-zA-Z ]{3,15}$/, '*Please enter valid name'),
+        email: yup.string().email('*Please enter valid email').required('*Please fill this field'),
+        // phone: yup.string().matches(/^[0-9]{10,10}$/, '*Phone number is not valid'),
+        phone: yup.string().matches(/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/, '*Phone number is not valid').required('*Please fill this field'),
+        date: yup.date().min(date, '*Past date not allowed').required('*Please fill this field'),
         department: yup.string().required(),
-        message: yup.string().min(20, 'Please enter minimum 20 charachter').max(100, 'Maximum 100 charachter are allowed').required('Please fill this field')
+        message: yup.string()
+            .matches(/^\S+(?: \S+)*$/, 'Extra spaces are not allowed')
+            .min(20, '*Please enter minimum 20 charachter')
+            .max(100, '*Maximum 100 charachter are allowed')
+            .required('*Please fill this field')
+            .test("message", "Only 5 Word are allowed", function (val) {
+                if (val.split(' ').length <= 10) {
+                    return true
+                } else {
+                    return false
+                }
+            }),
+        file: yup.mixed()
+            .required()
+            .test("FILE_SIZE", "Uploaded file is too big.",
+                value => !value || (value && value.size <= 2))
+            .test("FILE_FORMAT", "Uploaded file has unsupported format.",
+                value => !value || (value && jpg.includes(value.type)))
     });
 
     const formikObj = useFormik({
@@ -21,15 +42,27 @@ function Appointment(props) {
             phone: '',
             date: '',
             department: '',
-            message: ''
+            message: '',
+            file: ''
         },
         onSubmit: values => {
+            let arr = values.message.split(" ");
+
+            let arrOne = arr.map((v) => {
+                return v[0].toUpperCase() + v.substring(1)
+            })
+            let arrTwo = arrOne.join(" ")
+            values.message = arrTwo
+            console.log(arrTwo);
+
             console.log(values);
+            alert(JSON.stringify(values, null, 2));
+            handleReset()
         },
         validationSchema: appointmentSchema
     })
 
-    const { handleSubmit, handleBlur, handleChange, errors, touched, values } = formikObj;
+    const { handleSubmit, handleBlur, handleChange, handleReset, errors, touched, values } = formikObj;
 
 
     return (
@@ -56,7 +89,7 @@ function Appointment(props) {
                                 onChange={handleChange}
                                 value={values.name}
                             />
-                            {errors.name && touched.name ? <span>{errors.name}</span> : null}
+                            {errors.name && touched.name ? <span className='error'>{errors.name}</span> : null}
                             <div className="validate" />
                         </div>
                         <div className="col-md-4 form-group mt-3 mt-md-0">
@@ -72,7 +105,7 @@ function Appointment(props) {
                                 onChange={handleChange}
                                 value={values.email}
                             />
-                            {errors.email && touched.email ? <span>{errors.email}</span> : null}
+                            {errors.email && touched.email ? <span className='error'>{errors.email}</span> : null}
                             <div className="validate" />
                         </div>
                         <div className="col-md-4 form-group mt-3 mt-md-0">
@@ -88,7 +121,7 @@ function Appointment(props) {
                                 onChange={handleChange}
                                 value={values.phone}
                             />
-                            {errors.phone && touched.phone ? <span>{errors.phone}</span> : null}
+                            {errors.phone && touched.phone ? <span className='error'>{errors.phone}</span> : null}
                             <div className="validate" />
                         </div>
                     </div>
@@ -106,7 +139,7 @@ function Appointment(props) {
                                 onChange={handleChange}
                                 value={values.date}
                             />
-                            {errors.date && touched.date ? <span>{errors.date}</span> : null}
+                            {errors.date && touched.date ? <span className='error'>{errors.date}</span> : null}
                             <div className="validate" />
                         </div>
                         <div className="col-md-4 form-group mt-3">
@@ -118,13 +151,22 @@ function Appointment(props) {
                                 onChange={handleChange}
                                 value={values.department}
                             >
-                                <option value>Select Department</option>
+                                <option value=''>Select Department</option>
                                 <option value="Department 1">Department 1</option>
                                 <option value="Department 2">Department 2</option>
                                 <option value="Department 3">Department 3</option>
                             </select>
-                            {errors.department && touched.department ? <span>{errors.department}</span> : null}
+                            {errors.department && touched.department ? <span className='error'>{errors.department}</span> : null}
                             <div className="validate" />
+                        </div>
+                        <div className="col-md-4 form-group mt-3">
+                            <InputBox
+                                type="file"
+                                name="file"
+                                className="form-control"
+                                value={values.file}
+                            />
+                            {errors.file && touched.file ? <span className='error'>{errors.file}</span> : null}
                         </div>
                     </div>
                     <div className="form-group mt-3">
@@ -136,9 +178,9 @@ function Appointment(props) {
                             defaultValue={""}
                             onBlur={handleBlur}
                             onChange={handleChange}
-                            value={values.message.charAt(0).toUpperCase() + values.message.slice(1)}
+                            value={values.message}
                         />
-                        {errors.message && touched.message ? <span>{errors.message}</span> : null}
+                        {errors.message && touched.message ? <span className='error'>{errors.message}</span> : null}
                         <div className="validate" />
                     </div>
                     <div className="mb-3">
