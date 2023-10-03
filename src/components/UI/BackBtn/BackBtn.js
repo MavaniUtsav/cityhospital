ab9740d2e894f474d625960f56508d3cf4740c8e
MavaniUtsav <utsavmavani01@gmail.com>
2023-10-03 15:02:30 +0530

    Done

--- src/components/UI/BackBtn/BackBtn.js
+++ src/components/UI/BackBtn/BackBtn.js
@@ -3,6 +3,8 @@ import Button from '../Button/Button';
 import { useNavigate } from 'react-router-dom';
 
 function BackBtn(props) {
+
+
     const navigate = useNavigate()
 
     // 2 Type to add goBack functionality
